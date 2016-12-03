7983733792cf9b75daeb856b16fa47852cc80f45
Cesare Ghirelli <cghirelli@arpa.emr.it>
2016-12-04 00:47:26 +0100

    added more realism

--- main.py
+++ main.py
@@ -7,38 +7,56 @@ SCRABBLE_GLOBAL_CRATE = 'aaaaaaaaaaaabbbbcccccccddddeeeeeeeeeeee' \
                         'ffffgggghhiiiiiiiiiiillllllmmmmmmnnnnnnoo' \
                         'ooooooooooppppqqrrrrrrrssssssstttttttuuuuvvvvzz'
 
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.crate = []
+        self.words = []
+        self.score = 0
+        self.solver = Solver(WORDFILE)
+
+    def pick_from_crate(self, global_crate):
+        picked = []
+        for _ in range(8 - len(self.crate)):
+            if len(global_crate) > 0:
+                choice = random.choice(global_crate)
+                global_crate.pop(global_crate.index(choice))
+                picked.append(choice)
+        self.crate = self.crate + picked
+        return global_crate
+
+    def play_one_move(self):
+        try:
+            found_word = self.solver.find_words(self.crate, "")[0]
+            self.words.append(found_word)
+            for char in found_word[0]:
+                self.crate.pop(self.crate.index(char))
+            self.score = sum([x[1] for x in self.words])
+            return True
+        except IndexError as exp:
+            return False
+
+
 if __name__ == '__main__':
     print("A game simulation")
     global_crate = list(SCRABBLE_GLOBAL_CRATE)
-    solver = Solver(WORDFILE)
-    player1_words, player2_words = [], []
-    for i in range(len(SCRABBLE_GLOBAL_CRATE)):
-        caret = []
-        for _ in range(8):
-            if len(global_crate) <= 0:
-                exit()
-            choice = random.choice(global_crate)
-            global_crate.pop(global_crate.index(choice))
-            caret.append(choice)
-        CROSSING_LETTERS = ''
-        if i % 2 == 0:
-            print("Player 1 CARET='%s'" % "".join(caret))
-            player1_words.append(solver.find_words(
-                "".join(caret), CROSSING_LETTERS)[0])
-        else:
-            print("Player 2 CARET='%s'" % "".join(caret))
-            player2_words.append(solver.find_words(
-                "".join(caret), CROSSING_LETTERS)[0])
-        if player1_words:
-            print("Player 1 word: %s" % player1_words[-1][0])
-            print("Player 1 total points: %s" %
-                  sum([x[1] for x in player1_words]))
-        if player2_words:
-            print("Player 2 word: %s" % player2_words[-1][0])
-            print("Player 2 total points: %s" %
-                  sum([x[1] for x in player2_words]))
-        print("Player 1 total maccarone: %s" % player1_words)
-        print("Player 2 total maccarone: %s" % player2_words)
+    player1 = Player("King Kong")
+    player2 = Player("Hulk")
+    play, count = True, 0
+    while play:
+        print("Turn %s" % str(count / 2))
 
-    print("Player 1 total maccarone: %s" % player1_words)
-    print("Player 2 total maccarone: %s" % player2_words)
+        if count % 2 == 0:
+            global_crate = player1.pick_from_crate(global_crate)
+            print("%s crate: %s, len: %s, score: %s, words: %s" %
+                  (player1.name, player1.crate, len(player1.crate), player1.score, player1.words))
+            play = player1.play_one_move()
+        else:
+            global_crate = player2.pick_from_crate(global_crate)
+            print("%s crate: %s, len: %s, score: %s, words: %s" %
+                  (player2.name, player2.crate, len(player2.crate), player2.score, player2.words))
+            play = player2.play_one_move()
+        print("Global crate: %s" % global_crate)
+        count += 1
