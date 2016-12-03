c1ffa2ef438f5cb0a80fdfbff2c3cb00f5aedb4e
Cesare Ghirelli <cghirelli@arpa.emr.it>
2016-12-03 23:36:35 +0100

    find most valuable word in a dictionary

---
+++ most_valuable_word_in_dictionary.py
@@ -0,0 +1,17 @@
+"""Scarabeo solver."""
+WORDFILE = 'dicts/italia-1a'
+import random
+from engine.solver import Solver
+
+SCRABBLE_GLOBAL_CRATE = 'aaaaaaaaaaaabbbbcccccccddddeeeeeeeeeeee' \
+                        'ffffgggghhiiiiiiiiiiillllllmmmmmmnnnnnnoo' \
+                        'ooooooooooppppqqrrrrrrrssssssstttttttuuuuvvvvzz'
+
+if __name__ == '__main__':
+    solver = Solver(WORDFILE)
+    CROSSING_LETTERS = ''
+    caret = SCRABBLE_GLOBAL_CRATE
+    print("CARET='%s'" % caret)
+    print("CROSSING_LETTERS='%s'" % CROSSING_LETTERS)
+    found_words = solver.find_words(caret, CROSSING_LETTERS)
+    print(found_words)[:20]
